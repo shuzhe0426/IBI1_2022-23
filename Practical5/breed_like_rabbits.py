ec2d94118bdfee293ca5292546c5bb65f3747749
shuzhe0426 <shuzhe.22@intl.zju.edu.cn>
2023-03-15 23:13:58 +0800

    rabbit!

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,1 +1,13 @@
-#I need to set the initial amount to represent the oldest generation of rabbits.
+#I should set a while-loop
+sum=2
+n=1
+while sum<100:
+    print("The number of ",n,"th generation births is ",sum)
+    sum*=2
+    n+=1
+print("There are more than 100 rabbits in the ",n,"th generation")
+print("More than 100 rabbits were born in the ",n,"th generation")
+	
+
+  
+      
