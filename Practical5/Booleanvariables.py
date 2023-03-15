0b8b1006191e67f8d0dd159ebf88291fc3d5b731
shuzhe0426 <shuzhe.22@intl.zju.edu.cn>
2023-03-15 17:37:07 +0800

    1

---
+++ Practical5/Booleanvariables.py
@@ -0,0 +1,6 @@
+X=True
+Y=False
+W=X and Y
+Z=X or Y
+print("W")
+print("Z")
---
+++ Practical5/hexagons.py
@@ -0,0 +1,4 @@
+#Repeat
+for n in range(1,6):
+#The formula for the nth hexagonal number is h=2*n*(2*n-1)/2
+print(2*n*(2*n-1)/2)
