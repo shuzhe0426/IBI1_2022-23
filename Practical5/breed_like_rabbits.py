b15429ab370a5715559bf4d1e977b415b90fb12d
shuzhe0426 <shuzhe.22@intl.zju.edu.cn>
2023-03-17 15:38:32 +0800

    add explanation

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,10 +1,14 @@
 #I should set a while-loop
+#sum means the total sum of rabbit
 sum=2
+#n means the generation
 n=1
+#set a while-loop
 while sum<100:
     print("The number of ",n,"th generation births is ",sum)
     sum*=2
     n+=1
+#show the output of the calculation
 print("There are more than 100 rabbits in the ",n,"th generation")
 print("More than 100 rabbits were born in the ",n,"th generation")
 	
