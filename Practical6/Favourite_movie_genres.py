cc586970f3dd14cc3c1360b31b23dbf3419c1de2
shuzhe0426 <shuzhe.22@intl.zju.edu.cn>
2023-03-22 13:39:11 +0800

    Fixed the wrong place

--- Practical6/Favourite_movie_genres.py
+++ Practical6/Favourite_movie_genres.py
@@ -1,8 +1,13 @@
+#create a dictionary
+#Store different types of movies and their corresponding data in this dictionary
+dict={"Comedy":"73", "Action":"42", "Romance":"38", "Fantasy":"28", "Science-fiction":"22", "Horror":"19", "Crime":"18", "Documentary":"12", "History":"8", "War":"7"}
+
 #create and print a dictionary in Python to record these data and construct a pie chart from the data
 #use pyplot in matplotlib
 import matplotlib.pyplot as plt
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = 'Comedy', 'Action', 'Romance', 'Fantacy', 'Science-fiction', 'Horror', 'Crime', 'Documentary', 'History', 'War'
+#input the sizes, colors,explode
 sizes = [27.34, 15.73, 14.23, 10.49, 8.23, 7.12, 6.74, 4.49, 3.00, 2.63]
 colors = 'yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'pink', 'lightsteelblue', 'aquamarine', 'thistle', 'mediumpurple', 'silver'
 explode = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
@@ -10,7 +15,14 @@ plt.pie(sizes,explode=explode,labels=labels,colors=colors,autopct='%1.1f%%',shad
 plt.axis('equal')
 plt.show()
 
-#create a dictionary
-my_dict={}
-genes={'Comedy':73, 'Action':42, 'Romance':38, 'Fantasy':28, "Science-fiction':22, 'Horror':19, 'Crime':18, 'Documentary':12, 'History':8, 'War':7}
+#The number	of university students who prefer a	movie	genre	taken	from the input list
+#use the dictionary I created
+value=dict["Comedy"]
+print(value)
+
+
+
+
+
+
 
--- Practical6/Olympic_costs.py
+++ Practical6/Olympic_costs.py
@@ -1,21 +1,26 @@
+#print a list of sorted vlue for the cost of hosting the Olympic games
+#Enter the value of costs
+costs=[1,8,15,7,5,14,43,40]
+#Enter the value of costs
+costs.sort()
+print(costs)
+
+#a bar plot displaying this sorted distribution of marks
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-
-data = [1,8,15,7,5,14,43,40]
-labels = ["Los Angeles 1984", "Seoul 1988", "Barcelona 1992", "Atlanta 1996", "Sydney 2000", "Athens 2003", "Beijing 2008", "London 2012"]
+#Enter the value of costs in ascending order
+data = [1,5,7,8,14,15,40,43]
+#Add the city and year corresponding to costs
+labels = ["Los Angeles 1984", "Sydney 2000", "Atlanta 1996", "Seoul 1988", "Athens 2003", "Barcelona 1992", "London 2012", "Beijing 2008"]
 colors = ["lightcoral","gold", "yellowgreen", "lightgreen", "lightseagreen", "deepskyblue", "dodgerblue", "mediumpurple"]
-
+#Use the values entered above and the elements to start drawing
+#Ensure the output of the bars
 plt.bar(range(len(data)),data,color=colors,width=0.5)
 plt.xticks(range(len(data)),labels)
+#Determine what the horizontal axis and the vertical axis look like
 plt.xlabel("Olympic Games")
 plt.ylabel("Costs")
 plt.show()
 
-sort() and ()
-reverse()
-costs=[1,8,15,7,5,14,43,40]
-sorted(costs)
-costs=sorted(costs)
-costs.sort()
-costs.reverse()
+
