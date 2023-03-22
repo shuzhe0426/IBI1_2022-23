85499b8b0073af58b77a56969bb7ba6cf45952e7
shuzhe0426 <shuzhe.22@intl.zju.edu.cn>
2023-03-22 15:55:42 +0800

    finish the task

--- Practical6/Favourite_movie_genres.py
+++ Practical6/Favourite_movie_genres.py
@@ -6,9 +6,9 @@ dict={"Comedy":"73", "Action":"42", "Romance":"38", "Fantasy":"28", "Science-fic
 #use pyplot in matplotlib
 import matplotlib.pyplot as plt
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'Comedy', 'Action', 'Romance', 'Fantacy', 'Science-fiction', 'Horror', 'Crime', 'Documentary', 'History', 'War'
+labels = list(dict.keys())
 #input the sizes, colors,explode
-sizes = [27.34, 15.73, 14.23, 10.49, 8.23, 7.12, 6.74, 4.49, 3.00, 2.63]
+sizes = list(dict.values())
 colors = 'yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'pink', 'lightsteelblue', 'aquamarine', 'thistle', 'mediumpurple', 'silver'
 explode = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 plt.pie(sizes,explode=explode,labels=labels,colors=colors,autopct='%1.1f%%',shadow=True,startangle=50)
@@ -17,8 +17,8 @@ plt.show()
 
 #The number	of university students who prefer a	movie	genre	taken	from the input list
 #use the dictionary I created
-value=dict["Comedy"]
-print(value)
+genres=dict["Comedy"]
+print(genres)
 
 
 
--- Practical6/Olympic_costs.py
+++ Practical6/Olympic_costs.py
@@ -1,18 +1,36 @@
+#make a dictionary
+dict={'Los Angeles 1984':1, 'Seoul 1988':8, 'Barcelona 1992':15, 'Atlanta 1996':7, 'Sydney 2000':5, 'Athens 2003':14, 'Beijing 2008':43, 'London 2012':40}
+
 #print a list of sorted vlue for the cost of hosting the Olympic games
 #Enter the value of costs
 costs=[1,8,15,7,5,14,43,40]
-#Enter the value of costs
+#Put the values of costs in ascending order and print them out
 costs.sort()
 print(costs)
 
+
 #a bar plot displaying this sorted distribution of marks
+#Define the city and the cost
+cities = ['Los Angeles 1984', 'Seoul 1988', 'Barcelona 1992', 'Atlanta 1996', 'Sydney 2000', 'Athens 2003', 'Beijing 2008', 'London 2012']
+costs = [1,8,15,7,5,14,43,40]
+#import the methods to be used
+import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-#Enter the value of costs in ascending order
-data = [1,5,7,8,14,15,40,43]
+#Enter the value of costs in ascending order 
 #Add the city and year corresponding to costs
-labels = ["Los Angeles 1984", "Sydney 2000", "Atlanta 1996", "Seoul 1988", "Athens 2003", "Barcelona 1992", "London 2012", "Beijing 2008"]
+cities = numpy.array(cities)
+costs = numpy.array(costs)
+inds = costs.argsort()
+sortedcities = cities[inds]
+print(sortedcities)
+#Import the cities into the lables
+labels = sortedcities
+#Import the value of the ascending costs into the data
+costs.sort()
+data = costs
+#Define the colors of bars
 colors = ["lightcoral","gold", "yellowgreen", "lightgreen", "lightseagreen", "deepskyblue", "dodgerblue", "mediumpurple"]
 #Use the values entered above and the elements to start drawing
 #Ensure the output of the bars
